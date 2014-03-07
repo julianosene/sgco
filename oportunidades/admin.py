625c1b24e3ad8846a477e8d146ad3197830e5789
Duda Nogueira <dudanogueira@gmail.com>
2014-03-06 22:16:24 -0300

    Registro dos demais modelos no admin

--- oportunidades/admin.py
+++ oportunidades/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from models import OportunidadeDeTrabalho
 from models import InscricaoOportunidade
+from models import TipoOportunidadeTrabalho
 
 
 # Register your models here.
 admin.site.register(OportunidadeDeTrabalho)
 admin.site.register(InscricaoOportunidade)
+admin.site.register(TipoOportunidadeTrabalho)
